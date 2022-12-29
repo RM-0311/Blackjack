c4b450ff51573021ad9be33640dda0f208411018
unknown <md.ridgeway.work@gmail.com>
2022-12-28 21:24:17 -0600

    Make UX Changes and add Realism

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
-import random
+from random import choice
 import os
 
 # Array to hold cards
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10,
+         10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+shuffled_cards = cards.copy()
 
-# Function that will add a card to a player/dealer's hand
+# Function that will shuffle cards when the number of cards is less than 10
+
+
+# Function that will add a card from shuffled_cards to a player/dealer's hand and remove it from the array
 
 
 def deal_card():
-    return random.choice(cards)
+    dealt_card = choice(shuffled_cards)
+    shuffled_cards.remove(dealt_card)
+    return dealt_card
+
+# Remove any face cards or aces
+
+
+# def normalize(card_list):
+#     if "A" in card_list:
+#         card_list.remove("A")
+#         card_list.append(11)
+#     elif "J" in card_list:
+#         card_list.remove("J")
+#         card_list.append(10)
+#     if "Q" in card_list:
+#         card_list.remove("Q")
+#         card_list.append(10)
+#     if "K" in card_list:
+#         card_list.remove("K")
+#         card_list.append(10)
+
 
 # Calculate the score of the given hand
 
 
 def calculate_score(card_list):
+
     if sum(card_list) == 21 and len(card_list) == 2:
         return 0
     elif sum(card_list) > 21 and 11 in card_list:
@@ -31,7 +57,7 @@ def compare(user_score, computer_score):
         return "Draw"
         game_over = True
     elif computer_score == 0:
-        return "You lose, computer has Blackjack!"
+        return "You lose, dealer has Blackjack!"
         game_over = True
     elif user_score == 0:
         return "You win, you got blackjack!"
@@ -40,7 +66,7 @@ def compare(user_score, computer_score):
         return "You busted, you lose."
         game_over = True
     elif computer_score > 21:
-        return "You win! The computer busted."
+        return "You win! The dealer busted."
         game_over = True
     elif user_score > computer_score:
         return "You win!"
@@ -53,27 +79,46 @@ def compare(user_score, computer_score):
 
 
 def game():
+
     user_cards = []
     computer_cards = []
     print("Dealing cards now...")
+    # print(shuffled_cards)
     for _ in range(2):
         user_cards.append(deal_card())
         computer_cards.append(deal_card())
 
     game_over = False
 
+    print(f"Your cards: {user_cards}")
+    print(f"The dealer's up card is {computer_cards[0]}")
+    # normalize(user_cards)
+    # normalize(computer_cards)
     while not game_over:
+
         user_score = calculate_score(user_cards)
         computer_score = calculate_score(computer_cards)
-        print(f"Your cards: {user_cards}")
+
         if computer_score == 0 or user_score == 21 or user_score > 21:
             game_over = True
         else:
-            hit = input(
-                "Would you like another card? Type 'y' for yes and 'n' for no'.")
-            if hit == 'y':
+            choice = input(
+                "Would you like to 'hit', 'split', 'double', or 'stay'?\n")
+            if choice == 'hit':
                 user_cards.append(deal_card())
-            else:
+                print(user_cards)
+            elif choice == 'split':
+
+                if user_cards[0] == user_cards[1]:
+                    second_hand = user_cards[1]
+                    user_cards.remove(user_cards[1])
+                    user_cards.append(deal_card())
+                    second_hand.append(deal_card())
+            elif choice == 'double':
+                # TODO: ADD BETTING, AND THEN THE OPTION FOR A PLAYER TO DOUBLE DOWN ON THEIR HAND, LIMITING THEM TO ONE CARD
+                user_cards.append(deal_card())
+                game_over = True
+            elif choice == 'stay':
                 game_over = True
 
     while computer_score != 0 and computer_score < 17:
@@ -85,10 +130,15 @@ def game():
     print(compare(user_score, computer_score))
 
 
-print("Welcome to Blackjack!")
+print("Welcome to Blackjack Practice!")
 game()
-if input("Would you like to restart the game? Type 'y' for yes, 'n' for no.") == 'y':
-    os.system('cls')
-    game()
-else:
-    exit()
+keep_playing = True
+while keep_playing:
+    if input("Would you like to play another hand? Type 'y' for yes, 'n' for no.\n") == 'y':
+        os.system('cls')
+        if len(shuffled_cards) < 10:
+            print("Shuffling now")
+            shuffled_cards = cards.copy()
+        game()
+    else:
+        exit()
